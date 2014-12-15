195c2b2a20e39d5fbbb655a50073253aa94a471a
Viacheslav Vorobey <vorobeyme@gmail.com>
2014-12-15 23:49:56 +0200

    Added assets

--- project/web/js/scripts.js
+++ project/web/js/scripts.js
@@ -1,27 +1,95 @@
-$(document).ready(function(){
+/**
+ * Eventmapia
+ * https://github.com/bionic-university/viacheslav-vorobey/tree/master/project
+ */
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+// EventsController
+var Events = {
 
-    function initialize() {
+    'viaCounter': 0,
 
-        /* position Amsterdam */
-        var latlng = new google.maps.LatLng(52.3731, 4.8922);
+    'init': function() {
+        $('#btn-add-via-point').click(Events.addViaPoint);
+        $(document).on('click', '.btn-delete', Events.deleteViaPoint);
+        $('#btn-add-refresh-direction').click(Events.refreshDirection);
+    },
 
-        var mapOptions = {
-            center: latlng,
-            scrollWheel: false,
-            zoom: 13
+    // Add via point
+    'addViaPoint' : function(e) {
+        e.preventDefault();
+        Events.viaCounter += 1;
+
+        $(this).parent().before('<div class="form-group"> <div class="form-inline">' +
+        '<input type="text" name="routeVia[]" placeholder="Via" class="form-control" id="via-autocomplete-input-'+ Events.viaCounter +'" style="width: 90%;">' +
+        '<a href="#" class="btn btn-danger btn-delete pull-right" style="font-size: 12px; line-height: 1.6;"><i class="glyphicon glyphicon-remove"></i> </a>' +
+        '</div></div>');
+
+        $('#map-canvas').css('position', 'fixed');
+    },
+
+    // Delete via point
+    'deleteViaPoint' : function(e) {
+        e.preventDefault();
+        $(this).parent().parent().remove();
+    },
+
+    // Refresh direction
+    'refreshDirection' : function(e) {
+        e.preventDefault();
+
+        var start = $('#place-autocomplete-input1').val();
+        var end = $('#place-autocomplete-input2').val();
+        var selectedMode = $('input[type=radio]:checked').val();
+        console.log(selectedMode);
+        var request = {
+            origin: start,
+            destination: end,
+            travelMode: google.maps.TravelMode[selectedMode]
         };
-
-        var marker = new google.maps.Marker({
-            position: latlng,
-            url: '/',
-            animation: google.maps.Animation.DROP
+        directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
         });
+    }
+};
 
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        marker.setMap(map);
 
-    };
+// CalendarController
+var Calendar = {
 
-});
+    'renderCalendar' : function(calendarId) {
+        calendarId.fullCalendar({
+            timeFormat: 'H(:mm)',
+            lang: 'en',
+            contentHeight: 550,
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek'   //agendaDay'
+            },
+            eventLimit: true,               // allow "more" link when too many events
+
+            eventMouseover: function(calEvent, jsEvent, view) {
+                var tooltip = '<div class="event-tooltip">' + calEvent.description + '</div>';
+                $("body").append(tooltip);
+                $(this).mouseover(function(e) {
+                    $(this).css('z-index', 10000);
+                    $('.event-tooltip').fadeIn('500');
+                    $('.event-tooltip').fadeTo('10', 1.9);
+                }).mousemove(function(e) {
+                    $('.event-tooltip').css('top', e.pageY + 10);
+                    $('.event-tooltip').css('left', e.pageX + 20);
+                });
+            },
+
+            eventMouseout: function(calEvent, jsEvent) {
+                $(this).css('z-index', 8);
+                $('.event-tooltip').remove();
+            },
+
+            events: '/web/calendar/events'
+        })
+    }
+
+};
